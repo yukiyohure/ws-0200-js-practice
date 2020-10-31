1151681a0b31c9cea67057df810013757bfd8f2a
yukiyohure <yukiyohure@gmail.com>
2020-10-31 17:28:37 +0900

    solve 003_algorithm/001_recursion

--- 003_algorithm/001_recursion.js
+++ 003_algorithm/001_recursion.js
@@ -11,6 +11,8 @@
  */
 
 function sumSequence (n, sum = 0) {
+  if (n === 0) return sum;
+  return sumSequence(n - 1, sum + n)
 }
 
 /**
@@ -21,9 +23,24 @@ function sumSequence (n, sum = 0) {
  *
  *  example:
  *    input: 10 => [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+ *
+ * @param {number} num - フィボナッチ数列をどこまで計算するかを指定
+ * @param {number} index - フィボナッチ数を入れる配列の場所を指定
+ * @param {array} array - フィボナッチ数を入れる配列
+ *
  */
 
 function fibonacci (num, index = 0, array = []) {
+  if (num === 0) return array; // numが0になったら再起を終了、加算してきたarrayを返す: この関数ではnumが再起の回数を担う
+
+  if (index === 0 || index === 1) { // 答えとなるフィボナッチ数列の１,2番目は必ず1になるので配列[0]と[1]には1を固定で入れる
+    array.push(1); // 配列の末尾に1を追加
+    return fibonacci(num -1, index + 1, array);
+  }
+
+  // フィボナッチ数列の漸化式 → An = An−1 + An−2
+  array.push(array[index - 1] + array[index - 2]);
+  return fibonacci(num - 1, index + 1, array); // 再起呼び出し
 }
 
 
@@ -80,6 +97,15 @@ function fibonacci (num, index = 0, array = []) {
  */
 
 function fileSize (node, sum = 0) {
+  // basecase
+  if ( !node.children && node.size ) return sum + node.size; // childrenがない = 子要素がない場合は最終的な和を返す
+
+  return node.children.reduce((acc, cur) => { // nodeのchildrenの中の配列をreduceで回す
+    if (cur.children) { // childrenの配列の要素の中にさらにchildrenがあった場合、↓
+      return acc + fileSize(cur, acc); // そのnode(cur)と今現在のファイル合計サイズの値を引数に指定し、再起呼び出しを行う
+    }
+    return acc + cur.size; // もうcurにchildrenが見つからない場合、「そのnode(cur)のsize」と「今までの合計値(acc)」の和を返す
+  }, 0);
 }
 
 
